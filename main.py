face4e5176c340368002c32ca6be043df792f466
sidling1 <p.jethwani@iitg.ac.in>
2024-04-13 23:59:22 +0530

    Added Users

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import re
 from difflib import SequenceMatcher
 
+
 def tokenize(code):
     # Remove comments before tokenizing
     code = re.sub(r'//.*?\n|/\*.*?\*/', '', code, flags=re.DOTALL)
     # Tokenize the code using regular expressions
     return re.findall(r'\b\w+\b', code)
+
 
 def compare_code(code1, code2):
     tokens1 = tokenize(code1)
@@ -14,6 +16,7 @@ def compare_code(code1, code2):
     # Compare token lists
     similarity_ratio = SequenceMatcher(None, tokens1, tokens2).ratio()
     return similarity_ratio
+
 
 def main():
     # Read two C code files
@@ -27,6 +30,7 @@ def main():
     similarity_ratio = compare_code(code1, code2)
 
     print("Similarity Ratio:", similarity_ratio)
+
 
 if __name__ == "__main__":
     main()
