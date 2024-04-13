b9e3415a16945bdfa5c9fb3bbd0d9cb7b1e8b6c4
tnp-iitg <iitg.tnp@gmail.com>
2024-04-14 00:56:02 +0530

    adding navbar

--- autograder/settings.py
+++ autograder/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+import os
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
